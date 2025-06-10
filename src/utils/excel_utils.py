aaec411aadeab101e52b8533f6bae46af5ed7b6b
Onshelf <118708458+Onshelf@users.noreply.github.com>
2025-06-10 18:16:18 +0300

    Delete src/utils directory

--- src/utils/excel_utils.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-def read_names_from_excel(file_path: str, column: str = 'Name') -> list:
-    """Read names from Excel column"""
-    df = pd.read_excel(file_path)
-    return df[column].dropna().tolist()
